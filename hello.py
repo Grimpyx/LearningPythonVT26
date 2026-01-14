b3941d69e36ceec300b090381ae0f0790c53b68c
Jesper Fagerberg <jesperfagerberg@live.se>
2026-01-14 12:33:39 +0100

    Added hellop.py. Edited gitignore.
    Ensured AI tools are disabled in preferences.

---
+++ hello.py
@@ -0,0 +1,4 @@
+print("Hello World") # So original!
+# pressing "Play" confirms it works.
+# As instructed in the assignments module in Canvas, it runs using CMD python as well:
+# I use windows explorer to navigate to the folder, RMB, using the context menu I open this folder in CMD, then type "python hello TAB TAB" -> "python .\hello.py"
