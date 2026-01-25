63f6ccb483bc2edec5c3a80241db496583f0c2b3
Jesper Fagerberg <jesperfagerberg@live.se>
2026-01-25 19:19:41 +0100

    Completed 8.1

---
+++ 8 Files/8.1 Filesum.py
@@ -0,0 +1,11 @@
+def filesum(fileName):
+    file = open(fileName)
+    lines = file.readlines()
+    file.close()
+
+    sum = 0
+    for line in lines:
+        sum += int(line)
+    return sum
+
+print(filesum("LearningPythonVT26/8 Files/Testing.txt"))
